8e73c104d30d0291b60f5d5d4699416ccfcc6b8e
Pavan Kalyan <pavankalyannidamanuri@gmail.com>
2024-04-06 19:06:59 +0530

    added sample data

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express=require('express');
+import sampleData from './usersList';
 const app=express();
 
-app.get("/",(req,res)=>{
-    res.send({
-        name:"pavan",
-        age:25
-    })
+app.get("/users",(req,res)=>{
+    res.send(sampleData)
 })
 
 app.listen(3000,(req,res)=>{
---
+++ usersList.js
@@ -0,0 +1,9 @@
+const sampleData = [
+    { id: 1, name: "John Doe", email: "john@example.com", age: 30 },
+    { id: 2, name: "Jane Smith", email: "jane@example.com", age: 25 },
+    { id: 3, name: "Michael Johnson", email: "michael@example.com", age: 35 },
+    { id: 4, name: "Emily Davis", email: "emily@example.com", age: 28 },
+    { id: 5, name: "David Brown", email: "david@example.com", age: 40 },
+  ];
+  
+  export default sampleData;
