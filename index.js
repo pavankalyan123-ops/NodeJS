e0c2a8b5566860eb0ba9f7b9695e9d194121628d
Pavan Kalyan <pavankalyannidamanuri@gmail.com>
2024-04-11 09:30:10 +0530

    adding id functionality

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ app.get("/users",(req,res)=>{
     res.send(sampleData)
 })
 
-
+app.get("/users/:id",(req,res)=>{
+    const id=req.params.id;
+    const data=sampleData.filter(d=>d.id==id);
+    if(data!==undefined){
+        res.json(data)
+    }else{
+        res.status(404).json({ error: "This ID does not exist" });
+    }
+})
 app.listen(5000,(req,res)=>{
     
     console.log("port is running in 5000")
