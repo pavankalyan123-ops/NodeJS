14ad184dfa15576f10a48a7deb94f6f44273d2cf
Pavan Kalyan <pavankalyannidamanuri@gmail.com>
2024-04-16 18:57:09 +0530

    added few

--- index.js
+++ index.js
@@ -3,10 +3,24 @@ const express=require('express');
 const sampleData=require('./usersList')
 const app=express();
 
-app.get("/users",(req,res)=>{
-    console.log(req.query)
-    res.json(sampleData)
-})
+// app.get("/users",(req,res)=>{
+//     if(Object.keys(req.query).length>0){
+//         let searchValue=req.query.search;
+//         let data=sampleData.filter(d=>d.name.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase()));
+//         res.json(data);
+//     }
+//     res.json(sampleData)
+// })
+
+app.get("/users", (req, res) => {
+    let data = sampleData;
+    if (Object.keys(req.query).length > 0 && req.query.search) {
+        let searchValue = req.query.search.toLowerCase();
+        data = sampleData.filter(d => d.name.toLowerCase().includes(searchValue));
+    }
+    res.json(data);
+});
+
 
 app.get("/users/:id",(req,res)=>{
     const id=req.params.id;
